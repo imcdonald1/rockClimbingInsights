e575ac927e51856127fe17be102c33e0125053c2
imcdonald1 <imcdonald1@mail.csuchico.edu>
2019-12-19 12:01:09 -0800

    knit to html and operation discussion

--- part2.r
+++ part2.r
@@ -1,20 +1,20 @@
-## ----one----------------------------------------------------------------------------------------------------
+## ----one----------------------------------------------------------------------
 library("knitr")
 library("caret")
 
 
-## ----two----------------------------------------------------------------------------------------------------
+## ----two----------------------------------------------------------------------
 purl("part1.Rmd", output = "part1.r")
 source("part1.r")
 
 
-## ----three--------------------------------------------------------------------------------------------------
+## ----three--------------------------------------------------------------------
 myvars <- c("grade_id", "height", "weight", "started", "sex")
 grade_model_data <- hardestWithYear[myvars]
 grade_model_data <- na.omit(grade_model_data)
 
 
-## ----four---------------------------------------------------------------------------------------------------
+## ----four---------------------------------------------------------------------
 set.seed(926)
 sample <- sample.int(n = nrow(grade_model_data), 
                      size = floor(.90*nrow(grade_model_data)), # Selecting 90% of data
@@ -24,16 +24,16 @@ train_data <- grade_model_data[sample, ]
 test_data  <- grade_model_data[-sample, ]
 
 
-## ----five---------------------------------------------------------------------------------------------------
+## ----five---------------------------------------------------------------------
 grade_model <- lm(train_data, formula = grade_id ~ height + weight + started + sex)
 summary(grade_model)
 
 
-## ----seven--------------------------------------------------------------------------------------------------
+## ----seven--------------------------------------------------------------------
 injuries = read.csv("climbInjuries.csv", header = TRUE)
 
 
-## ----eight--------------------------------------------------------------------------------------------------
+## ----eight--------------------------------------------------------------------
 new_per_year <- count(userInfo, year_started)
 colnames(new_per_year)[colnames(new_per_year) == "year_started"] <- "year"
 colnames(new_per_year)[colnames(new_per_year) == "n"] <- "num_new_climbers"
@@ -43,11 +43,11 @@ new_per_year <- na.omit(new_per_year)
 injuries <- merge(injuries,new_per_year, by="year")
 
 
-## ----nine---------------------------------------------------------------------------------------------------
+## ----nine---------------------------------------------------------------------
 colnames(injuries)
 
 
-## ----ten----------------------------------------------------------------------------------------------------
+## ----ten----------------------------------------------------------------------
 injury_plot <- ggplot(injuries, aes(x=year)) + geom_point(aes(y=num_new_climbers, colour ='blue')) +
     geom_point(aes(y=num_of_accidents, colour ='red')) + ylab("Number of Climbers") + xlab("Year") +    scale_color_manual(labels = c("# of New Climbers", "# of Accidents"), values = c("blue", "red"))
 
